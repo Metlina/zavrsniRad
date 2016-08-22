dde48d3231895fb2163c0f5991c49b4959733efc
Tino Petrina <tinopetrina@live.com>
2016-08-22 18:48:58 +0200

    added new project

---
+++ Redux.Sample/Redux.Sample/Redux.SamplePage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+
+namespace Redux.Sample
+{
+    public partial class Redux.SamplePage : ContentPage
+	{
+		public Redux.SamplePage ()
+		{
+
+            InitializeComponent();
+}
+	}
+}
+
